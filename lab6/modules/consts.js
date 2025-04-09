01707eb2dec94ee1880b37befd1b3016e59e706c
tal3nt3d <afonin05@yandex.ru>
2025-04-09 18:17:15 +0300

    added labs 1-6

---
+++ lab6/modules/consts.js
@@ -0,0 +1,3 @@
+export const groupId = 59336460
+export const accessToken = 'vk1.a.62cB1tPgb-LeWI-UhvV4SMv9PoRLXnj-A9yLRSMQV9EZ4w3fPCeYlb8cQTjwyAWxSKlb6MukxqXM401RQfFfpMTzaKMOLdWJCBZ9wSYUBfqGnYXwCpyQ7GggNX89sXnyFT8i8X2yjOS8wsKYoxsekMJOo-_YXuZrN0Vh_yy2U0zLtApGwenjM7NpIp_UWcwSgbX-WGEJo_dTFMdC7E8hKA'
+export const version = '5.199'
